0a55396ee672e37bef6d9bfd37d3664b68aecf67
mehmettberke <dxd3d9@gmail.com>
2024-12-01 01:57:07 +0300

    add credit

--- src/app/api/addCredit/route.ts
+++ src/app/api/addCredit/route.ts
@@ -1,3 +1,50 @@
-export async function POST() {
-  console.log("sa");
+import { createClient } from "@supabase/supabase-js";
+import { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+// Singleton pattern for Prisma client
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const userId = body.userId;
+  const requestedCredit = body.requestedCredit || 1;
+
+  try {
+    // Prisma ile kredi güncelleme
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        credit: {
+          increment: requestedCredit,
+        },
+      },
+      select: {
+        credit: true,
+      },
+    });
+
+    return new Response(
+      JSON.stringify({
+        message: "Credit added successfully",
+        newCreditTotal: updatedUser.credit,
+      }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Prisma error:", error);
+    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
 }
