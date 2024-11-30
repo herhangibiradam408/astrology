4ff7ff565f77d5cab4fb29deee028203ba5be785
mehmettberke <dxd3d9@gmail.com>
2024-12-01 00:36:08 +0300

    prisma update

--- src/app/api/useCredit/route.ts
+++ src/app/api/useCredit/route.ts
@@ -1,7 +1,13 @@
 import { NextRequest } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
