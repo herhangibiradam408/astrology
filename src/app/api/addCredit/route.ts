bf80539650b553b1be1aa0b5725be8ce228894f5
mehmettberke <dxd3d9@gmail.com>
2024-12-01 01:30:16 +0300

    add credits

--- src/app/api/addCredit/route.ts
+++ src/app/api/addCredit/route.ts
@@ -1,62 +1,28 @@
 import { NextRequest } from "next/server";
-import prisma from "@/lib/prisma";
+import { addUserCredit } from "@/app/utils/creditManagement";
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-
-    // Input validation
     const userId = body.userId;
-    if (!userId) {
-      return new Response(JSON.stringify({ message: "User ID is required" }), {
-        status: 400,
+    const requestedCredit = body.requestedCredit || 1;
+
+    const result = await addUserCredit(userId, requestedCredit);
+
+    if (!result.success) {
+      return new Response(JSON.stringify({ message: result.error }), {
+        status: 500,
       });
     }
-
-    const requestedCredit = Number(body.requestedCredit) || 1;
-    if (isNaN(requestedCredit)) {
-      return new Response(
-        JSON.stringify({ message: "Invalid credit amount" }),
-        { status: 400 }
-      );
-    }
-
-    const updatedUser = await prisma.user.update({
-      where: {
-        id: userId,
-      },
-      data: {
-        credit: {
-          increment: requestedCredit,
-        },
-      },
-      select: {
-        credit: true,
-      },
-    });
 
     return new Response(
       JSON.stringify({
         message: "Credit added successfully",
-        newCreditTotal: updatedUser.credit,
+        newCreditTotal: result?.data?.credit,
       }),
-      {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
+      { status: 200 }
     );
-  } catch (error: any) {
-    console.error("Error processing request:", error);
-
-    // Check for specific Prisma errors
-    if (error.code === "P2025") {
-      return new Response(JSON.stringify({ message: "User not found" }), {
-        status: 404,
-      });
-    }
-
+  } catch (error) {
     return new Response(JSON.stringify({ message: "Internal Server Error" }), {
       status: 500,
     });
