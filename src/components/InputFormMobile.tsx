114f2ab5d6f212b1b1fcb6c61b1664677d4376d4
herhangibiradam408 <herhangibiradam408@gmail.com>
2025-09-29 16:39:52 +0300

    Update InputFormMobile.tsx

--- src/components/InputFormMobile.tsx
+++ src/components/InputFormMobile.tsx
@@ -24,7 +24,7 @@ function InputFormMobile({
             //width: "calc(22/100 * 100%)",
             width: `${inputWidth}px`,
           }}
-          className={`absolute md:text-[calc(8/400*100dvh)] text-[calc(7/400*100dvh)] tracking-tighter xl:top-[calc(87/100*100%)] md:top-[calc(86/100*100%)] xl:left-[calc(85/200*100%)] md:left-[calc(78/200*100%)] top-[calc(86/100*100dvh)] left-[calc(7/50*100dvh)] leading-tight -translate-y-2/3 bg-transparent border-none outline-none focus:border-none focus:outline-none text-white z-30 resize-none overflow-hidden`}
+          className={`absolute md:text-[calc(8/400*100dvh)] text-[calc(7/400*100dvh)] tracking-tighter xl:top-[calc(87/100*100%)] md:top-[calc(86/100*100%)] xl:left-[calc(85/200*100%)] md:left-[calc(78/200*100%)] top-[calc(86/100*100dvh)] left-[calc(7/50*100dvh)] leading-tight -translate-y-2/3 bg-transparent border-none outline-none focus:border-none focus:outline-none text-white z-30 resize-none`}
         >
           <div className="flex items-center gap-2">
             <p>YOUR NAME:</p>
@@ -65,7 +65,7 @@ function InputFormMobile({
               }}
               className="bg-transparent border-b mb-1.5 border-white focus:border-b focus:border-white focus:outline-none h-full"
             />
-            <Button onClick={handleClick} className="bg-transparent h-[10px]">
+            <Button onClick={handleClick} className="bg-transparent h-[10px] px-0 py-0">
               Send
             </Button>
           </div>
